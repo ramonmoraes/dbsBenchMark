6ec417b398fc36f72adf19a183248274db548f65
Ramon Moraes <ramon.moraes94@gmail.com>
2019-03-28 09:13:50 -0300

    Add fake data (#10)
    * create cleanNames csv python
    
    * add initial gitignore
    
    * update doc
    
    * add cleanLawsuit script
    
    * ignoring lawsuit.csvs
    
    * modify gitignore
    
    * update readme

---
+++ scripts/cleanLawsuits.py
@@ -0,0 +1,17 @@
+import json
+
+names_path = './data/lawsuits.csv'
+with open(names_path, 'r') as names_file:
+    lines = names_file.readlines()
+    lawsuits = list(
+        map(
+            lambda row: row.split('|')[-1].strip(),
+            lines
+        )
+    )[1:]
+
+    jsonDict = { "lawsuits_numbers": lawsuits }
+    lawsuit_json_path = "./data/lawsuits.json"
+
+    with open(lawsuit_json_path, 'w') as file:
+        file.write(json.dumps(jsonDict))
---
+++ scripts/cleanNamesCsv.py
@@ -0,0 +1,15 @@
+import csv
+import json
+
+namesPath = './data/nomes.csv'
+csvDicts = csv.DictReader(open(namesPath), "first_name")
+
+nameList = list(
+    map(lambda obj: dict(obj).get('f'),csvDicts)
+)
+
+namesJsonPath = './data/names.json'
+jsonDict = { "names": nameList[1:] }
+
+with open(namesJsonPath, 'w') as file:
+    file.write(json.dumps(jsonDict))
