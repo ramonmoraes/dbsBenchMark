20f7b02490bb62e09037af14da21061ec5df6b6e
Ramon Moraes <ramon.moraes94@gmail.com>
2019-03-28 10:57:06 -0300

    models with types

--- modules/models/models.py
+++ modules/models/models.py
@@ -1,34 +1,39 @@
-class Lawsuit:
-    number = ""
-    judges = ""
-    lawyers = []
-    related_people = []
-    court_section = "" ## Segunda turma do colegial
-    disctrict = "" # SSA, BH
-    subject = "" #(Ex: “Inclusão Indevida em Cadastro de Inadimplentes”, “Obrigação de Fazer”)
-    kind = "" # Direito do Trabalho", "Direito Penal"
+from schematics.types import StringType, ListType, BooleanType, ModelType
+from schematics.models import Model
 
-class Person:
-    name = ""
-
+class Person(Model):
+    name = StringType()
 
 class Judge(Person):
-    is_judge = True
+    is_judge = BooleanType()
 
 
-class Lawyers(Person):
-    is_lawyer = True
+class Lawyer(Person):
+    is_lawyer = BooleanType()
 
 
-class Disctrict:
-    name = ""
-    estate = ""
+class Disctrict(Model):
+    name = StringType()
+    estate = StringType()
 
-class CourtSection:
-    name = ""
 
-class Subject:
-    name = ""
+class CourtSection(Model):
+    name = StringType()
 
-class Kind:
-    name = ""
+
+class Subject(Model):
+    name = StringType()
+
+
+class Kind(Model):
+    name = StringType()
+
+class Lawsuit(Model):
+    number = StringType()
+    judge = ModelType(Judge)
+    lawyers = ListType(ModelType(Lawyer))
+    related_people = ListType(ModelType(Person))
+    court_section = ModelType(CourtSection) ## Segunda turma do colegial
+    disctrict = ModelType(Disctrict) # SSA, BH
+    subject = ModelType(Subject) #(Ex: “Inclusão Indevida em Cadastro de Inadimplentes”, “Obrigação de Fazer”)
+    kind = ModelType(Kind) # Direito do Trabalho", "Direito Penal"
