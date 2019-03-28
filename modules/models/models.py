eca95e039a60ba253ef9c932170d0d0285f8a506
Ramon Moraes <ramon.moraes94@gmail.com>
2019-03-28 10:59:05 -0300

    add black and format files

--- modules/models/models.py
+++ modules/models/models.py
@@ -1,8 +1,10 @@
 from schematics.types import StringType, ListType, BooleanType, ModelType
 from schematics.models import Model
 
+
 class Person(Model):
     name = StringType()
+
 
 class Judge(Person):
     is_judge = BooleanType()
@@ -28,12 +30,15 @@ class Subject(Model):
 class Kind(Model):
     name = StringType()
 
+
 class Lawsuit(Model):
     number = StringType()
     judge = ModelType(Judge)
     lawyers = ListType(ModelType(Lawyer))
     related_people = ListType(ModelType(Person))
-    court_section = ModelType(CourtSection) ## Segunda turma do colegial
-    disctrict = ModelType(Disctrict) # SSA, BH
-    subject = ModelType(Subject) #(Ex: “Inclusão Indevida em Cadastro de Inadimplentes”, “Obrigação de Fazer”)
-    kind = ModelType(Kind) # Direito do Trabalho", "Direito Penal"
+    court_section = ModelType(CourtSection)  ## Segunda turma do colegial
+    disctrict = ModelType(Disctrict)  # SSA, BH
+    subject = ModelType(
+        Subject
+    )  # (Ex: “Inclusão Indevida em Cadastro de Inadimplentes”, “Obrigação de Fazer”)
+    kind = ModelType(Kind)  # Direito do Trabalho", "Direito Penal"
--- scripts/cleanNamesCsv.py
+++ scripts/cleanNamesCsv.py
@@ -1,15 +1,13 @@
 import csv
 import json
 
-namesPath = './data/nomes.csv'
+namesPath = "./data/nomes.csv"
 csvDicts = csv.DictReader(open(namesPath), "first_name")
 
-nameList = list(
-    map(lambda obj: dict(obj).get('f'),csvDicts)
-)
+nameList = list(map(lambda obj: dict(obj).get("f"), csvDicts))
 
-namesJsonPath = './data/names.json'
-jsonDict = { "names": nameList[1:] }
+namesJsonPath = "./data/names.json"
+jsonDict = {"names": nameList[1:]}
 
-with open(namesJsonPath, 'w') as file:
+with open(namesJsonPath, "w") as file:
     file.write(json.dumps(jsonDict))
