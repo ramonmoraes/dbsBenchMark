941bf24ec66f1b6230c3b3b2c8a8f438f5e69f8e
Ramon Moraes <ramon.moraes94@gmail.com>
2019-03-31 21:07:41 -0300

    add schemaCreator

---
+++ modules/dbs/dgraph/schemaCreator.py
@@ -0,0 +1,41 @@
+SCHEMA_TEMPLATE = """ {label}: {type} ."""
+
+class SchemaCreator:
+    def __init__(self, models):
+        self.models = models
+
+
+    def get_identifier(self, model):
+        identifier_label = "is_{}".format(model.__class__.__name__.lower())
+        return SCHEMA_TEMPLATE.format(
+            label=identifier_label,
+            type=self.get_type(True)
+        )
+
+
+
+    def create_schema(self):
+        schemas = []
+        for model in self.models:
+            schemas.append(
+                self.get_identifier(model)
+            )
+            for k, v in model.to_primitive().items():
+                schema = SCHEMA_TEMPLATE.format(
+                    label=k,
+                    type=self.get_type(v)
+                )
+                schemas.append(schema)
+        return list(set(schemas))
+
+
+
+    def get_type(self, raw):
+        val = type(raw)
+        if val == type(""):
+            return "string"
+        if val == type(1):
+            return "double"
+        if val == type(True):
+            return "bool"
+        raise Exception("Complex schema", raw)
