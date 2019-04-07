9407e4dc246328df5bfc1fefbbeb32d6bf8bbb21
Ramon Moraes <ramon.moraes94@gmail.com>
2019-04-07 16:04:14 -0300

    create basic csv using batch

--- modules/dbs/sql/csvCreator.py
+++ modules/dbs/sql/csvCreator.py
@@ -2,52 +2,67 @@ import csv
 from modules.relatedModels import get_related_model, load_dataset
 from modules.dbs.sql.tableCreator import get_table_name
 from modules.models.models import *
+from itertools import islice, chain
 
-csv_path = "data/csv"
+def list_batch(iterable, n=10000):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
+
+
+CSV_PATH = "data/csv"
+
+
 class CsvCreator:
     def __init__(self):
         data = load_dataset()
-        self.related_people = map(lambda n: Person({"name": n}), data.get('related_peoples'))
-        self.lawyers = map(lambda n: Lawyer({"name": n}), data.get('lawyers'))
-        self.judges = map(lambda n: Judge({"name": n}), data.get('judges'))
-        self.kinds = map(lambda n: Kind({"name": n}), data.get('kinds'))
-        self.lawsuits_numbers = data.get('lawsuits')
+        self.related_people = list(map(
+            lambda n: Person({"name": n}), data.get("related_peoples")
+        ))
+        self.lawyers = list(map(lambda n: Lawyer({"name": n}), data.get("lawyers")))
+        self.judges = list(map(lambda n: Judge({"name": n}), data.get("judges")))
+        self.kinds = list(map(lambda n: Kind({"name": n}), data.get("kinds")))
+        self.lawsuits_numbers = data.get("lawsuits")
+        self._meta_data = {
+            "sizes": {
+                "related_people": len(self.related_people),
+                "lawyers": len(self.lawyers),
+                "judges": len(self.judges),
+                "kinds": len(self.kinds),
+            }
+        }
+
+
+    def iter_from_attribte(self, key):
+        for d in self.__dict__.get("key"):
+            yield d
 
     def create_basic_csv(self):
         d = self.__dict__
-        del d['lawsuits_numbers']
+        del d["lawsuits_numbers"]
+        del d["_meta_data"]
 
         for k, v in d.items():
             print("Writting :", k)
-            with open("{}/{}.csv".format(csv_path, get_table_name(k)), 'w') as f:
-                dicts = list(
-                    map(lambda m: m.to_primitive(), v)
-                )[1:10]
-                w = csv.DictWriter(f, dicts[0].keys())
-                w.writeheader()
-                w.writerows(dicts)
+            for batch in list_batch(k, 10):
+                with open("{}/{}.csv".format(CSV_PATH, get_table_name(k)), 'a') as f:
+                    dicts = list( map(lambda m: m.to_primitive(), v) )
+                    for dikt in list_batch(dicts, 3):
+                        w = csv.DictWriter(f, dicts[0].keys())
+                        w.writeheader()
+                        w.writerows(dicts)
 
 CsvCreator().create_basic_csv()
 
-# def get_model_random_amount(model, arr, max):
-#     res = []
-#     for x in range(random.randint(1, max)):
-#         res.append(
-#             model({"name": random.choice(arr)})
-#         )
-#     return res
-
-
-
-    # def create(self, percent = 100):
-    #     max_csv_size = len(self.lawsuits) * percent / 100
-    #     if max_csv_size < 1:
-    #         print("Can not create less then 1 csv")
-    #         return
-    #     print("Getting {} csv".format(max_csv_size))
-    #     csv_count = 0
-    #     for lawsuit in self.lawsuits:
-    #         if csv_count>=max_csv_size:
-    #             return
-    #         csv_count+=1
-#CsvCreator().create(0.0001)
+# def create(self, percent = 100):
+#     max_csv_size = len(self.lawsuits) * percent / 100
+#     if max_csv_size < 1:
+#         print("Can not create less then 1 csv")
+#         return
+#     print("Getting {} csv".format(max_csv_size))
+#     csv_count = 0
+#     for lawsuit in self.lawsuits:
+#         if csv_count>=max_csv_size:
+#             return
+#         csv_count+=1
+# CsvCreator().create(0.0001)
---
+++ modules/dbs/sql/insertData.py
@@ -0,0 +1,42 @@
+import csv
+from modules.relatedModels import get_related_model, load_dataset
+from modules.dbs.sql.tableCreator import get_table_name
+
+
+def format_col_values(val):
+    if type(val) == type("str"):
+        return '"{}"'.format(val)
+    if type(val) == type(True):
+        return str(val).lower()
+    return val
+
+
+def create_query(table, objs):
+    if type(objs) != type([]):
+        objs = [objs]
+
+    queries = []
+    for obj in objs:
+        query_template = "INSERT INTO {table} ({columns}) ({values});"
+        columns = obj.keys()
+        values = list(map(format_col_values, obj.values()))
+
+        columns = " ,".join(columns)
+        values = " ,".join(values)
+        queries.append(
+            query_template.format(table=table, columns=columns, values=values)
+        )
+    return queries
+
+
+def get_queires():
+    for lawsuit in get_related_model(0.0001)[1:2]:
+        data = lawsuit.to_primitive()
+        law_dict = {"number": data.pop("number")}
+        queries = []
+        for k, v in data.items():
+            if v:
+                queries.extend(create_query(get_table_name(k), v))
+        queries.extend(create_query(get_table_name("lawsuit"), law_dict))
+        for q in queries:
+            print(q)
