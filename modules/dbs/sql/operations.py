a8aa2682e4882ef11c2dade961aff6464a67c77d
Ramon Moraes <ramon.moraes94@gmail.com>
2019-04-21 12:21:00 -0300

    recreate dabase and load from csv working

--- modules/dbs/sql/operations.py
+++ modules/dbs/sql/operations.py
@@ -1,5 +1,30 @@
 from modules.dbs.sql.connection import connection
+from os.path import abspath
 
+def print_cursor(cursor):
+    for res in cursor.fetchall():
+        print(res)
+
+def recreate_database():
+    cursor = connection.cursor()
+    commands = """SET FOREIGN_KEY_CHECKS = 0;
+SET @tables = NULL;
+SELECT GROUP_CONCAT(table_schema, '.', table_name) INTO @tables
+  FROM information_schema.tables
+  WHERE table_schema = 'db';
+
+SET @tables = CONCAT('DROP TABLE ', @tables);
+PREPARE stmt FROM @tables;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+SET FOREIGN_KEY_CHECKS = 1; """
+    querys = commands.split(";")
+    querys = filter(lambda x: x.strip(), querys)
+    querys = map(lambda s: "{}{}".format(s, ";"), querys)
+    for querie in querys:
+        print("Querie:", querie)
+        cursor.execute(querie)
+        connection.commit()
 
 def create_tables():
     base_config_path = "modules/dbs/sql/config/"
@@ -23,3 +48,25 @@ def create_tables():
         print("Executing command:", command)
         cursor = connection.cursor()
         cursor.execute(command)
+
+def load_data():
+    cursor = connection.cursor()
+    print("Loading")
+    command = """
+LOAD DATA LOCAL INFILE "data/csv/kindsTable.csv"
+INTO TABLE kindTable
+COLUMNS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS
+(name);
+    """
+    cursor.execute(command)
+    print_cursor(cursor)
+    connection.commit()
+    command = cursor.execute("select name from kindTable limit 10;")
+    print_cursor(cursor)
+
+recreate_database()
+create_tables()
+load_data()
