3020048f37b68b76fabe2094191e7b134214f681
Ramon Moraes <ramon.moraes94@gmail.com>
2019-04-21 09:53:03 -0300

    padding lines

--- modules/dbs/sql/config/simple_tables.sql
+++ modules/dbs/sql/config/simple_tables.sql
@@ -2,23 +2,33 @@ CREATE TABLE IF NOT EXISTS personTable (
     id INT AUTO_INCREMENT,
     name varchar(255),last_name varchar(255),
     PRIMARY KEY (id)
-);CREATE TABLE IF NOT EXISTS lawyerTable (
+);
+
+CREATE TABLE IF NOT EXISTS lawyerTable (
     id INT AUTO_INCREMENT,
     name varchar(255),last_name varchar(255),is_lawyer boolean,
     PRIMARY KEY (id)
-);CREATE TABLE IF NOT EXISTS judgeTable (
+);
+
+CREATE TABLE IF NOT EXISTS judgeTable (
     id INT AUTO_INCREMENT,
     name varchar(255),last_name varchar(255),is_judge boolean,
     PRIMARY KEY (id)
-);CREATE TABLE IF NOT EXISTS kindTable (
+);
+
+CREATE TABLE IF NOT EXISTS kindTable (
     id INT AUTO_INCREMENT,
     name varchar(255),
     PRIMARY KEY (id)
-);CREATE TABLE IF NOT EXISTS courtsectionTable (
+);
+
+CREATE TABLE IF NOT EXISTS courtsectionTable (
     id INT AUTO_INCREMENT,
     name varchar(255),
     PRIMARY KEY (id)
-);CREATE TABLE IF NOT EXISTS disctrictTable (
+);
+
+CREATE TABLE IF NOT EXISTS disctrictTable (
     id INT AUTO_INCREMENT,
     name varchar(255),estate varchar(255),
     PRIMARY KEY (id)
