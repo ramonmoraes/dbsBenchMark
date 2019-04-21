b90033643434b2980775f1b52da3ea4a8ea967a8
Ramon Moraes <ramon.moraes94@gmail.com>
2019-04-21 12:50:34 -0300

    benckmark calling sqlOperations

--- modules/benchmark.py
+++ modules/benchmark.py
@@ -1,27 +1,37 @@
+from modules.dbs.sql.operations import SqlOperations
+
 class BenchMark:
     def __init__(self, amount = 1):
         self.amount = amount
+        self.sqlOps = SqlOperations()
 
-    def start():
+    def start(self):
         self.create_dbs()
         self.insert_datas()
         self.make_queries()
         self.compare_results()
 
-    def create_dbs():
+    def create_dbs(self):
         self.create_mysql_db()
         self.create_dgraph_db()
 
-    def insert_data():
+    def insert_data(self):
         self.insert_mysql_data()
         self.insert_dgraph_data()
 
-    def make_queries():
-        self.sql_results = make_mysql_queries()
-        self.dgraph_results = make_dgraph_queries()
+    def make_queries(self):
+        self.sql_results = self.make_mysql_queries()
+        self.dgraph_results = self.make_dgraph_queries()
 
-    def compare_results():
+    def compare_results(self):
         print(
             self.sql_results
             self.dgraph_results
         )
+
+    def create_mysql_db(self):
+        self.sqlOps.recreate_database()
+        self.sqlOps.create_tables()
+
+    def insert_mysql_data(self):
+        self.sqlOps.load_data()
