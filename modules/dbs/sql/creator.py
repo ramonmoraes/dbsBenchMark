2ff2daf8ff6a831b273e8e75c2688027db8f36c8
Ramon Moraes <ramon.moraes94@gmail.com>
2019-04-21 09:55:11 -0300

    csv creator creating related lawsuit

--- modules/dbs/sql/creator.py
+++ modules/dbs/sql/creator.py
@@ -28,6 +28,7 @@ class Creator:
         self.lawsuits_numbers = data.get("lawsuits")
         self._meta_data = {
             "sizes": {
+                "lawsuits_numbers": len(self.lawsuits_numbers),
                 "related_people": len(self.related_people),
                 "lawyers": len(self.lawyers),
                 "judges": len(self.judges),
@@ -45,24 +46,49 @@ class CsvCreator(Creator):
             print("Writting :", k)
             for batch in list_batch(k, 500000):
                 file_path = "{}/{}.csv".format(CSV_PATH, get_table_name(k))
-                with open(file_path, 'a') as f:
-                    dicts = list( map(lambda m: m.to_primitive(), v) )
+                dicts = list( map(lambda m: m.to_primitive(), v) )
+                self.write_csv(file_path, dicts)
+
+
+    def write_csv(self, file_path, dicts):
+        with open(file_path, 'a') as f:
                     w = csv.DictWriter(f, dicts[0].keys())
                     w.writeheader()
                     w.writerows(dicts)
 
-# CsvCreator().create_basic_csv()
-tc = TableCreator([Lawsuit])
 
-lawsuit_query_template = """
-INSERT INTO {table} (number, judge_id, kind_id) VALUES ("{number}", {judge_id}, {kind_id});
-"""
+    def get_random_id(self, key):
+        return random.randint(1, self._meta_data['sizes'][key])
+
+    def create_lawsuit_csv(self):
+        lawsuits = self.__dict__['lawsuits_numbers']
+        lawsuit_path = "data/csv/lawsuits.csv"
+        for lawsuit_batch in list_batch(lawsuits, 500000):
+            related_lawsuits = []
+            for lawsuit_number in lawsuit_batch:
+                lawsuit = {
+                    "number": lawsuit_number,
+                    "judge_id": self.get_random_id('judges'),
+                    "kind_id": self.get_random_id('kinds'),
+                }
+                related_lawsuits.append(lawsuit)
+            self.write_csv(lawsuit_path, related_lawsuits)
+
+
+    def create_related_csv(self):
+        print("Writting lawsuit csv")
+        self.create_lawsuit_csv()
+
+
+CsvCreator().create_related_csv()
+
 
 #TODO: Add UNIQUE TO SIMPLE TABLES!!!!
 class DataCreator(Creator):
+    TC = TableCreator([Lawsuit])
     def get_id(self, model):
         return "(SELECT id FROM {table} WHERE {identifier}=\"{value}\")".format(
-            table = tc.get_table_name(model),
+            table = self.TC.get_table_name(model),
             identifier = model.keys()[0],
             value = model.values()[0]
         )
@@ -72,6 +98,7 @@ class DataCreator(Creator):
         f.close()
 
     def create_data(self, max_data=1):
+        lawsuit_query_template = """INSERT INTO {table} (number, judge_id, kind_id) VALUES ("{number}", {judge_id}, {kind_id});"""
         data_path = 'data/insert_queries.sql'
         self.clean_file(data_path)
         data_count = 0
@@ -91,16 +118,14 @@ class DataCreator(Creator):
                 kind = random.choice(self.kinds)
 
                 for model in [judge, kind]:
-                    insert_queries.extend(create_query(tc.get_table_name(model), model))
+                    insert_queries.extend(create_query(self.TC.get_table_name(model), model))
 
                 insert_queries.append(
                     lawsuit_query_template.format(
-                        table=tc.get_table_name(Lawsuit()),
+                        table=self.TC.get_table_name(Lawsuit()),
                         number=lawsuit_number,
                         kind_id=self.get_id(kind),
                         judge_id=self.get_id(judge)
                     )
                 )
                 data_count+=1
-
-DataCreator().create_data()
