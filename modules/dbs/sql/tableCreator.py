f36ed236b1978f20a080951808ff1c2333544245
Ramon Moraes <ramon.moraes94@gmail.com>
2019-04-27 11:44:33 -0300

    moved create simple table to tableCreator module

--- modules/dbs/sql/tableCreator.py
+++ modules/dbs/sql/tableCreator.py
@@ -1,3 +1,14 @@
+from modules.models.models import (
+    Person,
+    Lawyer,
+    Judge,
+    Kind,
+    CourtSection,
+    Disctrict,
+    Subject,
+)
+from modules.dbs.sql.tableCreator import TableCreator
+
 TABLE_TEMPLATE = """CREATE TABLE IF NOT EXISTS {table_name} (
     id INT AUTO_INCREMENT,
     {columns},
@@ -37,3 +48,21 @@ class TableCreator:
         if val == type(True):
             return "boolean"
         raise Exception("Complex table", raw)
+
+def create_simple_tables():
+    tc = TableCreator(
+        models=[
+            Person(),
+            Lawyer(),
+            Judge(),
+            Kind(),
+            CourtSection(),
+            Disctrict(),
+            Subject(),
+        ]
+    )
+    tables = tc.create_tables()
+    print("Creating simple tables")
+    module_path = "modules/dbs/sql/config/simple_tables.sql"
+    with open(module_path, "w") as f:
+        f.writelines(tables)
