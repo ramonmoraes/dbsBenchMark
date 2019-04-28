8bd40f8108fe627d5ae589c2ca3cd9e86f6d7e5c
Ramon Moraes <ramon.moraes94@gmail.com>
2019-04-28 20:55:31 -0300

    main beign main

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
-from modules.benchmark import BenchMark
-
+# from modules.benchmark import BenchMark
 # benchmark = BenchMark()
 # benchmark.create_mysql_db()
 # benchmark.insert_mysql_data()
+
+from modules.dbs.dgraph.creator import DgraphCreator
+c = DgraphCreator()
+
+c.write_nquads()
+
+# from modules.dbs.dgraph.operations import DgraphOperation
+
+# # ops = DgraphOperation()
+# # ops.drop_all()
+
+# client = DgraphOperation().client
+
+
+# import pydgraph
+# # schema = 'name: string @index(exact) .'
+# # op = pydgraph.Operation(schema=schema)
+
+
+# txn = client.txn()
+# try:
+#     # res = txn.query(
+#     #     """
+#     #         {
+#     #             data(func: has(name)) {
+#     #                 name
+#     #             }
+#     #         }
+#     #     """
+#     # )
+#     # print(res)
+#     txn.mutate(set_nquads=nquads)
+# #     schema = 'name: string @index(exact) .'
+# #     op = pydgraph.Operation(schema=schema)
+#     txn.commit()
+# finally:
+#     txn.discard()
