143a5e9ff26e4f219a22f026d933f41783dc34e7
Ramon Moraes <ramon.moraes94@gmail.com>
2019-04-28 16:15:40 -0300

    add high level abstraction on benchmark

--- modules/benchmark.py
+++ modules/benchmark.py
@@ -1,9 +1,11 @@
 from modules.dbs.sql.operations import SqlOperations
+from modules.dbs.dgraph.operations import DgraphOperations
 
 class BenchMark:
     def __init__(self, amount = 1):
         self.amount = amount
         self.sqlOps = SqlOperations()
+        self.dgraphOps = DgraphOperations()
 
     def start(self):
         self.create_dbs()
@@ -32,6 +34,10 @@ class BenchMark:
     def create_mysql_db(self):
         self.sqlOps.recreate_database()
         self.sqlOps.create_tables()
+
+    def create_dgraph_db(self):
+        self.dgraphOps.drop_all()
+        self.dgraphOps.create_index()
 
     def insert_mysql_data(self):
         self.sqlOps.load_data()
