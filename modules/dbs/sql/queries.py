3d5f6e1d263c8520af8fd1784ea6f8e7e33a9251
Ramon Moraes <ramon.moraes94@gmail.com>
2019-05-05 19:46:36 -0300

    add find_every_related_data sql query

--- modules/dbs/sql/queries.py
+++ modules/dbs/sql/queries.py
@@ -11,4 +11,20 @@ class SqlQueries(Queries):
         pass
 
     def find_every_related_data(self):
+        query = """
+        SELECT num.number, kin.name, jud.name as "judgeName", law.name as "lawyerName", per.name as "personName"
+        FROM lawsuitTable as num
+        LEFT JOIN kindTable as kin
+        ON num.kind_id = kin.id
+        LEFT JOIN judgeTable as jud
+        on jud.id = num.judge_id
+        LEFT JOIN lawsuitlawyerTable as ll
+        on num.id = ll.lawsuit_id
+        LEFT JOIN lawyerTable as law
+        on law.id = ll.lawyer_id
+        LEFT JOIN lawsuitpersonTable as lp
+        on num.id = ll.person_id
+        LEFT JOIN lawyerTable as per
+        on per.id = lp.person_id
+        """
         pass
