d58c614668db1a5019a4398a54526e1dd391c714
Ramon Moraes <ramon.moraes94@gmail.com>
2019-05-05 09:58:59 -0300

    create querie abstract class

---
+++ modules/dbs/queries.py
@@ -0,0 +1,19 @@
+"""
+Queries to be made
+
+1 - Find the judge related with more lawsuits
+2 - Find the top 5 relations judge x kind
+3 - Find the top 5 relations judge x lawyers
+
+Running each querie 4 times
+"""
+
+class Queries:
+    def find_judge_with_more_lawsuits(self):
+        raise Exception("Method have to be implemented")
+
+    def find_top_five_relations_judge_kind(self):
+        raise Exception("Method have to be implemented")
+
+    def find_top_five_relations_judge_lawyers(self):
+        raise Exception("Method have to be implemented")
