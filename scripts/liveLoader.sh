916993a84914670a492b272b36b1f7691ca7056e
Ramon Moraes <ramon.moraes94@gmail.com>
2019-05-11 12:05:08 -0300

    add bulk on live loader

--- scripts/liveLoader.sh
+++ scripts/liveLoader.sh
@@ -1,3 +1,4 @@
 dgraphId=$(docker ps | grep zero | awk '{print $1}')
 echo $dgraphId
-docker exec $dgraphId sh -c "dgraph live -r ./data/nquad/lawsuitsSmall.txt --zero localhost:5080 -d server:9080"
+# docker exec $dgraphId sh -c "dgraph live -r data/nquad/lawsuits.rdf --zero localhost:5080 -d server:9080"
+docker exec $dgraphId sh -c "dgraph bulk -r data/nquad/lawsuits.rdf -s data/nquad/schema.txt --zero=localhost:5080"
