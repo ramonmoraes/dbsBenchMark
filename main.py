78da4691b704adf7207aa53cf36ccb76542d4ca0
Ramon Moraes <ramon.moraes94@gmail.com>
2019-05-12 11:38:55 -0300

    main calling only start

--- main.py
+++ main.py
@@ -2,14 +2,3 @@ from modules.benchmark import BenchMark
 
 benchmark = BenchMark()
 benchmark.start()
-
-# benchmark.create_dgraph_db()
-# benchmark.create_dgraph_data()
-# benchmark.pre_scripts()
-# benchmark.configure()
-# benchmark.insert_dgraph_data()
-# benchmark.make_queries()
-
-
-# benchmark.create_mysql_db()
-# benchmark.insert_mysql_data()
